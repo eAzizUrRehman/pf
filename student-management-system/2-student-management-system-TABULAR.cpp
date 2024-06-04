aa42877c7862cdc89bc80220552854f656968e0b
Aziz Ur Rehman <eAzizUrRehman@gmail.com>
2024-06-05 00:29:58 +0500

    implement delete functionality

--- student-management-system/2-student-management-system-TABULAR.cpp
+++ student-management-system/2-student-management-system-TABULAR.cpp
@@ -29,6 +29,113 @@ void addStudentRecord(Student students[], int &count, int maxSize)
     cout << "\nStudent record added successfully." << endl;
 }
 
+void deleteStudentRecordByIndex(Student students[], int &count, int maxSize)
+{
+    if (count < 1)
+    {
+        cout << "\n\tNo student records to delete." << endl;
+        return;
+    }
+
+    int index;
+
+    cout << "\n\tEnter student's index to delete: ";
+
+    cin >> index;
+
+    if (index >= maxSize)
+    {
+        cout << "\n\tNo student found at the given index." << endl;
+        return;
+    }
+
+    // Shift the elements of the array
+    for (int j = index; j < count - 1; j++)
+        students[j] = students[j + 1];
+
+    cout << "\n\tStudent record deleted successfully." << endl;
+    count--;
+}
+
+void deleteStudentRecordByName(Student students[], int &count)
+{
+    if (count < 1)
+    {
+        cout << "\nNo student records to delete." << endl;
+        return;
+    }
+
+    string nameToDelete;
+
+    cout << "\n\tEnter student's name to delete: ";
+
+    cin >> nameToDelete;
+
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        if (students[i].name == nameToDelete)
+            break;
+    }
+
+    if (i == count)
+    {
+        cout << "\n\tNo student found with the given name." << endl;
+        return;
+    }
+
+    // Shift the elements of the array
+    for (int j = i; j < count - 1; j++)
+        students[j] = students[j + 1];
+
+    cout << "\n\tStudent record deleted successfully." << endl;
+    count--;
+}
+
+void deleteStudentRecordByRollNo(Student students[], int &count)
+{
+    if (count < 1)
+    {
+        cout << "\nNo student records to delete." << endl;
+        return;
+    }
+
+    int rollNoToDelete;
+
+    cout << "\n\tEnter student's roll no to delete: ";
+
+    cin >> rollNoToDelete;
+
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        if (students[i].rollNumber == rollNoToDelete)
+            break;
+    }
+
+    if (i == count)
+    {
+        cout << "\n\tNo student found with the given roll number." << endl;
+        return;
+    }
+
+    // Shift the elements of the array
+    for (int j = i; j < count - 1; j++)
+        students[j] = students[j + 1];
+
+    cout << "\n\tStudent record deleted successfully." << endl;
+    count--;
+}
+
+void deleteAllStudentsRecords(Student students[], int &count){
+    count = 0; // reset the count
+
+    cout << "\n\tAll student records deleted successfully." << endl;
+
+}
+
+
+
 // Function to display all student records in the database
 void displayAllStudentRecords(const Student students[], int count)
 {
@@ -62,21 +169,47 @@ int main()
         cout << "\nMain Menu:\n";
         cout << "\t1. Add Student Record\n";
         cout << "\t2. Display All Student Records\n";
-        cout << "\t3. Exit\n";
+        cout << "\t3. Delete a Student Record [By Index]\n";
+        cout << "\t4. Delete a Student Record [By Name]\n";
+        cout << "\t5. Delete a Student Record [By Roll Number]\n";
+        cout << "\t6. Delete All Student Records\n";
+        cout << "\t99. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
-        if (choice == 1)
-            addStudentRecord(students, count, maxSize);
-        else if (choice == 2)
-            displayAllStudentRecords(students, count);
-        else if (choice == 3)
+        switch (choice)
         {
+        case 1:
+            addStudentRecord(students, count, maxSize);
+            break;
+
+        case 2:
+            displayAllStudentRecords(students, count);
+            break;
+
+        case 3:
+            deleteStudentRecordByIndex(students, count, maxSize);
+            break;
+
+        case 4:
+            deleteStudentRecordByName(students, count);
+            break;
+
+        case 5:
+            deleteStudentRecordByRollNo(students, count);
+            break;
+
+        case 6:
+            deleteAllStudentsRecords(students, count);
+            break;
+
+        case 99:
             cout << "Exiting program. Thank you for using Student Management System!" << endl;
             break;
-        }
-        else
+
+        default:
             cout << "Invalid choice. Please enter again." << endl;
+        }
     }
 
     return 0;
